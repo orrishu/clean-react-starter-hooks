ec389151bff585328aed3480927cd5990b806988
Ori Shuchami <ori_s@ifat.com>
2021-03-07 12:53:07 +0200

    fixed less possible errors.

--- webpack.config.js
+++ webpack.config.js
@@ -134,7 +134,9 @@ module.exports = {
         }, {
           loader: 'less-loader',
           options: {
-            javascriptEnabled: true
+            lessOptions: {
+              javascriptEnabled: true
+            }
           }
         }]
       }, {
